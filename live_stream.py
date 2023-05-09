c8cc742dd039431d65f13611d0b42a0850eb6ed3
Niyigena-Yves <wizynives@gmail.com>
2023-05-09 23:41:43 +0200

    face-detection

---
+++ live_stream.py
@@ -0,0 +1,20 @@
+import cv2
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+counter = 0
+limit = 10
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,0), 2)
+        sub_face = img[y:y+h, x:x+w]
+        FaceFileName = "detected_faces/face_" + str(y) + ".jpg"
+        cv2.imwrite(FaceFileName, sub_face)
+        counter += 1
+    cv2.imshow('img',img)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
